83c54c007ed5030927f6f668cd68171aeaec4b5e
techchee <ketyung@techchee.com>
2021-03-24 20:11:56 +0800

    Initial Commit

---
+++ SwiftUIOAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIOAuth
+//
+//  Created by Chee Ket Yung on 24/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIOAuth/SwiftUIOAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOAuthApp.swift
+//  SwiftUIOAuth
+//
+//  Created by Chee Ket Yung on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
